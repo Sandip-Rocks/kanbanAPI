c2ec05a9d53aa2b34171137e5c1fb6d7ff6a126b
Sandip-Rocks <sandip4991@gmail.com>
2021-11-28 11:01:57 +0530

    'added_increment_model'

--- model/board.model.js
+++ model/board.model.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+const autoIncrementModelID = require('./counterModel');
 const kanbanSchema = new mongoose.Schema({
+   id: { type: Number, unique: true, min: 1 }, //increment by 1
   stage: {
     type: Number,
     default: 1,
@@ -8,6 +10,14 @@ const kanbanSchema = new mongoose.Schema({
     type: String,
     require: true,
   },
+});
+kanbanSchema.pre('save', function (next) {
+  if (!this.isNew) {
+    next();
+    return;
+  }
+
+  autoIncrementModelID('activities', this, next);
 });
 
 mongoose.model("Kanban", kanbanSchema);
